0847550d91a15a153a7590d0f59b2d11c94aa3cb
mike6321 <mike6321@naver.com>
2021-07-21 14:32:40 +0900

    (ex1-hello-jpa-remind) 프록시(3) - 타입비교

--- ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
@@ -23,17 +23,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("junwoo");
+            Member member1 = new Member();
+            member1.setUsername("junwoo");
 
-            em.persist(member);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("junwoo");
+
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("junwoo");
+
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.getReference(Member.class, member.getId());
-            System.out.println("findMember.getUsername() = " + findMember.getUsername());
-            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.find(Member.class, member2.getId());
+            Member m3 = em.getReference(Member.class, member3.getId());
+
+            System.out.println("m1 == m2 :: " + (m1.getClass() == m2.getClass()));
+            System.out.println("m1 == m3 :: " + (m1.getClass() == m3.getClass()));
+
+            logic(m1, m3);
 
             tx.commit();
         } catch (Exception e) {
@@ -41,6 +56,16 @@ public class JpaMain {
         }
         emf.close();
 
+    }
+
+    /**
+     * 실제로는 이렇게 쓰이기 때문에
+     * 프록시 타입인지 아닌지를 구분하기 힘들다.
+     * 그렇기 때문에 == 비교 대신 instanceof 를 사용하자!
+     * */
+    private static void logic(Member m1, Member m3) {
+        System.out.println(m1 instanceof Member);
+        System.out.println(m3 instanceof Member);
     }
 
 }
