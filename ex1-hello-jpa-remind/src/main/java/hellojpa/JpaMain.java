ef8be4a9a9f5d3c678b32164e75d3f9d3dad65fe
mike6321 <mike6321@naver.com>
2021-07-21 16:04:14 +0900

    (ex1-hello-jpa-remind) 프록시(5) - LazyInitializationException 예외

--- ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
@@ -26,52 +26,19 @@ public class JpaMain {
             member.setUsername("junwoo");
             em.persist(member);
 
-            Member newMember = new Member();
-            newMember.setUsername("junwoo");
-            em.persist(newMember);
-
-            Member newMember2 = new Member();
-            newMember2.setUsername("junwoo");
-            em.persist(newMember2);
-
             em.flush();
             em.clear();
 
-            Member member1 = em.find(Member.class, member.getId());
-            System.out.println("member1.getClass() = " + member1.getClass());
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("refMember.getClass() = " + refMember.getClass());
 
-            Member member2 = em.getReference(Member.class, member.getId());
-            System.out.println("member2.getClass() = " + member2.getClass());
+            em.clear();
 
-            /**
-             * JPA는 같은 영속성 컨텍스트 및 트랜잭션 내에서는
-             * 항상 == 비교시 true를 반환하게 설계되어 있다.
-             * */
-            System.out.println("member1 == member2 :: " + (member1 == member2));
+            refMember.getUsername();
 
-
-            /**
-             * 무조건 같은 영속성 컨텍스트에서는 == 이 true가 되게 강제한다.
-             * */
-            //step01
-            Member member3 = em.getReference(Member.class, newMember.getId());
-            System.out.println("member3.getClass() = " + member3.getClass());
-            Member member4 = em.getReference(Member.class, newMember.getId());
-            System.out.println("member4.getClass() = " + member4.getClass());
-
-            System.out.println("member3 == member4 :: " + (member3 == member4));
-
-            //step02
-            Member member5 = em.getReference(Member.class, newMember2.getId());
-            System.out.println("member5.getClass() = " + member5.getClass());
-            Member member6 = em.find(Member.class, newMember2.getId());
-            System.out.println("member6.getClass() = " + member6.getClass());
-
-            System.out.println("member5 == member6 :: " + (member5 == member6));
-
-            tx.commit();
         } catch (Exception e) {
             em.close();
+            e.printStackTrace();
         }
         emf.close();
 
