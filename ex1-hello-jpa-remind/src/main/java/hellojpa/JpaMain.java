b3cba39e9438226a54eeb4ea6070ad3ba3b1bef9
mike6321 <mike6321@naver.com>
2021-07-26 08:57:17 +0900

    (ex1-hello-jpq-remind) chore(코드정리)

--- ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
@@ -42,52 +42,10 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            /**
-             * 아래 쿼리를 보면 Member 만 가지고 온것을 볼 수 있다.
-             * -> 기본적으로 지연로딩
-             *     select
-             *         member0_.MEMBER_ID as MEMBER_I1_7_0_,
-             *         member0_.city as city2_7_0_,
-             *         member0_.street as street3_7_0_,
-             *         member0_.zipcode as zipcode4_7_0_,
-             *         member0_.USERNAME as USERNAME5_7_0_
-             *     from
-             *         Member member0_
-             *     where
-             *         member0_.MEMBER_ID=?
-             *
-             *  @Embedded
-             *     private Address homeAddress;
-             *  이것은 멤버의 소속된 값 타입이기 때문에 같이 불러와진다.
-             *
-             *
-             * */
             System.out.println("*********************************");
             Member findMember = em.find(Member.class, member.getId());
             System.out.println("*********************************");
 
-
-            /**
-             * 원하는 값을 찾을 때 그제서야 쿼리가 발생하는 것을 볼 수 있다.
-             *     select
-             *         addresshis0_.MEMBER_ID as MEMBER_I1_0_0_,
-             *         addresshis0_.city as city2_0_0_,
-             *         addresshis0_.street as street3_0_0_,
-             *         addresshis0_.zipcode as zipcode4_0_0_
-             *     from
-             *         ADDRESS addresshis0_
-             *     where
-             *         addresshis0_.MEMBER_ID=?
-             * */
-            List<Address> addressHistory = findMember.getAddressHistory();
-            for (Address address : addressHistory) {
-                System.out.println("address = " + address.getCity());
-            }
-
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            for (String favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
 
             tx.commit();
         } catch (Exception e) {
