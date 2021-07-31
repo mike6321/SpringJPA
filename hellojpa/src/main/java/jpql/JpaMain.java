dc36ebe14ea7560106a808801ee4c5dbf444d51c
mike6321 <mike6321@naver.com>
2021-07-31 17:41:24 +0900

    (hellojpa) 기본 문법과 쿼리 API - 프로젝션(SELECT)

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -25,6 +25,58 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
+            /**
+             * 엔티티 프로젝션(1)
+             * result 또한 영속성 컨텍스트에 의해 관리된다.
+             * */
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                                    .getResultList();
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+
+            /**
+             * 엔티티 프로젝션(2)
+             * JOIN을 명시해야 유지보수 측면에서 좋다.
+             * */
+            List<Team> result2 = em.createQuery("select t from Member m join m.team t", Team.class)
+                                    .getResultList();
+
+            /**
+             * 임베디드 타입 프로젝션
+             * */
+            List<Address> result3 = em.createQuery("select o.address from Order o", Address.class)
+                                        .getResultList();
+
+            /**
+             * 스칼라 타입 프로젝션
+             * */
+            List result4 = em.createQuery("select m.username, m.age from Member m")
+                                .getResultList();
+
+
+            /**
+             * 여러가지 값을 가져올 때
+             * */
+            //step01
+            Object o = result4.get(0);
+            Object[] resultArray = (Object[]) o;
+            System.out.println("username = " + resultArray[0]);
+            System.out.println("age = " + resultArray[1]);
+
+            //step02
+            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
+                                            .getResultList();
+
+            //step03 DTO를 통해 호출
+            List<MemberDTO> resultList1 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = resultList1.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
 
             tx.commit();
         } catch (Exception e) {
