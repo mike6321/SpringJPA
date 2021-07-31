98a3410e6b4b6ae39d8aab0821643bc73fe02856
mike6321 <mike6321@naver.com>
2021-07-31 19:26:12 +0900

    (hellojpa) 조건식(CASE 등등)

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -35,8 +35,13 @@ public class JpaMain {
             em.clear();
 
             /**
-             *
+             * CASE 식
              * */
+            em.createQuery("select " +
+                                            "case when m.age <= 10 then '학생요금'" +
+                                                 "when m.age >= 60 then '경로요금'" +
+                                                 "else '일반요금' end "+
+                                     "from Member m");
 
             tx.commit();
         } catch (Exception e) {
