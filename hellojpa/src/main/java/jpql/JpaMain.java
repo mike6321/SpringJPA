35c1fc89b57e5d436fada206fdf87e7673e68189
mike6321 <mike6321@naver.com>
2021-07-31 17:08:03 +0900

    (hellojpa) 기본 문법과 쿼리 API - getResultList, getSingleResult

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Project : ex1-hello-jpa-remind
@@ -24,17 +25,22 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            /**
-             * TypedQuery
-             * 반환 타입이 명확할 때 사영
-             * */
+
             TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+
             /**
-             * Query
-             * 반환 타입이 명확하지 않을 때 사용
+             * getResultList
+             * 결과가 하나 이상일 때 리스트 반환
+             * 결과가 없으면 빈 리스트 반환
              * */
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            List<Member> resultList = query1.getResultList();
+            /**
+             * getSingleResult
+             * 걀과가 정확히 하나
+             * 결과가 없거나 둘 이상이면 예외 발생
+             * */
+            Member singleResult = query1.getSingleResult();
+
 
             tx.commit();
         } catch (Exception e) {
