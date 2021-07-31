952d3da57f07e2b925ed8f1ad240a068adadf21c
mike6321 <mike6321@naver.com>
2021-07-31 17:01:26 +0900

    (hellojpa) 기본 문법과 쿼리 API - TypedQuery, Query

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 /**
  * Project : ex1-hello-jpa-remind
@@ -22,7 +19,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
 
+            /**
+             * TypedQuery
+             * 반환 타입이 명확할 때 사영
+             * */
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            /**
+             * Query
+             * 반환 타입이 명확하지 않을 때 사용
+             * */
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         } catch (Exception e) {
