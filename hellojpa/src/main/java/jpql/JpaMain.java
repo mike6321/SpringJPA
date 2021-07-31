b2739716b97a1dc09a0b987ecf626dbc85293839
mike6321 <mike6321@naver.com>
2021-07-31 17:09:24 +0900

    code clear

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -25,21 +25,7 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-
             TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-
-            /**
-             * getResultList
-             * 결과가 하나 이상일 때 리스트 반환
-             * 결과가 없으면 빈 리스트 반환
-             * */
-            List<Member> resultList = query1.getResultList();
-            /**
-             * getSingleResult
-             * 걀과가 정확히 하나
-             * 결과가 없거나 둘 이상이면 예외 발생
-             * */
-            Member singleResult = query1.getSingleResult();
 
 
             tx.commit();
