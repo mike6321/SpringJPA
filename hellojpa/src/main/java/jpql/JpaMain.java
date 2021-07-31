0e4220817d59b69ac3d6554487292dc7f037d4ff
mike6321 <mike6321@naver.com>
2021-07-31 16:47:00 +0900

    (hellojpa) project init

---
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,36 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Project : ex1-hello-jpa-remind
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 4:03 오후
+ */
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+            e.printStackTrace();
+        }
+        emf.close();
+
+    }
+
+}
