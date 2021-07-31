cccc6f22ed9eb775c2e5fff9865c5d0e8e68ecc3
mike6321 <mike6321@naver.com>
2021-07-31 18:25:48 +0900

    code clear

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -20,13 +20,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member1" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            em.persist(member);
 
             em.flush();
             em.clear();
