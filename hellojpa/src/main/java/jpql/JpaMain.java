01c6022356571127101f796ee921db6fb29a21fa
mike6321 <mike6321@naver.com>
2021-07-31 18:04:11 +0900

    (hellojpa) 페이징

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -20,15 +20,31 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member1" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
 
             em.flush();
             em.clear();
 
+            /**
+             * 페이징
+             * 방언에 따라서 전략을 설정하여 페이징 쿼리가 나간다.
+             *
+             * */
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc ", Member.class)
+                                    .setFirstResult(1)
+                                    .setMaxResults(10)
+                                    .getResultList();
 
+            System.out.println("result.size" + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
