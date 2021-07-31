2e04c12cfdd8b7d6cc3b0f8175bf6e35e58e2af9
mike6321 <mike6321@naver.com>
2021-07-31 23:05:08 +0900

    code clear

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -20,28 +20,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("member");
-            member.setAge(10);
-            member.changeTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
-
-            /**
-             * CASE 식
-             * */
-            em.createQuery("select " +
-                                            "case when m.age <= 10 then '학생요금'" +
-                                                 "when m.age >= 60 then '경로요금'" +
-                                                 "else '일반요금' end "+
-                                     "from Member m");
 
             tx.commit();
         } catch (Exception e) {
