5b2eb50e08b37edcc1359618813caac518b42c08
mike6321 <mike6321@naver.com>
2021-08-01 11:27:07 +0900

    (hellojpa) code clear

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -51,40 +51,10 @@ public class JpaMain {
             String query = "select m from Member m";
             List<Member> result1 = em.createQuery(query, Member.class)
                                      .getResultList();
-            /**
-             * select
-             *             member0_.id as id1_0_,
-             *             member0_.age as age2_0_,
-             *             member0_.TEAM_ID as TEAM_ID5_0_,
-             *             member0_.type as type3_0_,
-             *             member0_.username as username4_0_
-             *         from
-             *             Member member0_*/
+
             for (Member member : result1) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-                //회원1, 팀A (SQL)
-                /**select
-                 team0_.id as id1_3_0_,
-                 team0_.name as name2_3_0_
-                 from
-                 Team team0_
-                 where
-                 team0_.id=?*/
-                //회원2, 팀A (1차캐시)
-                //회원3, 팀B (SQL)
-                /**select
-                 team0_.id as id1_3_0_,
-                 team0_.name as name2_3_0_
-                 from
-                 Team team0_
-                 where
-                 team0_.id=?*/
-
-                // 최악의 경우 회원이 100명인 경우 -> N + 1 = 101
             }
-
-
-
 
             tx.commit();
         } catch (Exception e) {
