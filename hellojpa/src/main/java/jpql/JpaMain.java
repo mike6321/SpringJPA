0bc1af6ac37c6215af84f05b5a75e4f83bef956e
mike6321 <mike6321@naver.com>
2021-08-01 11:11:45 +0900

    code clear

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -38,50 +38,6 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            /**
-             * 상태필드
-             * */
-            String query1 = "select m.username from Member m";
-            List<String> result1 = em.createQuery(query1, String.class)
-                                     .getResultList();
-
-            /**
-             * 단일 값 연관 경로
-             * : 묵시적 내부 조인 발생
-             * => 묵시적인 내부조인이 발생하지 않게 짜야한다.
-             *
-             select
-             team1_.id as id1_3_,
-             team1_.name as name2_3_
-             from
-             Member member0_
-             inner join
-             Team team1_
-             on member0_.TEAM_ID=team1_.id
-             **/
-            String query2 = "select m.team from Member m";
-            List<Team> result2 = em.createQuery(query2, Team.class)
-                                     .getResultList();
-
-            /**
-             * 컬렉션 값 연관 경로
-             * 묵시적 내부 조인 발생, 탐색 X
-             * */
-            String query3 = "select t.Members.size from Team t";
-            Integer result3 = em.createQuery(query3, Integer.class)
-                    .getSingleResult();
-            System.out.println("result3 = " + result3);
-
-            String query4 = "select t.Members from Team t";
-            Collection result4 = em.createQuery(query4, Collection.class)
-                                   .getResultList();
-            System.out.println("result4 = " + result4);
-
-            /**
-             * 명시적 조인
-             * */
-            String query5 = "select m.username from Team t join t.Members m";
-            em.createQuery(query5, Collection.class);
 
             tx.commit();
         } catch (Exception e) {
