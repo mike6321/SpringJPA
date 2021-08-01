c8eb0e8a5bc7d544e6e38affca0a6d4dcec58dc5
mike6321 <mike6321@naver.com>
2021-08-01 11:32:35 +0900

    (hellojpa) 페치조인(1) - ManyToOne 페치조인

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -48,12 +48,29 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m";
+            String query = "select m from Member m join fetch m.team";
             List<Member> result1 = em.createQuery(query, Member.class)
                                      .getResultList();
 
             for (Member member : result1) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                /**
+                 * 더이상 team은 프록시가 아니다.
+                 * -> 이미 member와 team의 데이터를 다가져왔다.
+                 *
+                 select
+                 member0_.id as id1_0_0_,
+                 team1_.id as id1_3_1_,
+                 member0_.age as age2_0_0_,
+                 member0_.TEAM_ID as TEAM_ID5_0_0_,
+                 member0_.type as type3_0_0_,
+                 member0_.username as username4_0_0_,
+                 team1_.name as name2_3_1_
+                 from
+                 Member member0_
+                 inner join
+                 Team team1_
+                 on member0_.TEAM_ID=team1_.id*/
             }
 
             tx.commit();
