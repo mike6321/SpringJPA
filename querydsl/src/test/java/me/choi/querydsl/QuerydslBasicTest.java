b4db822a0d49428f390544624ae19ce2c342775b
mike6321 <mike6321@naver.com>
2021-08-19 08:55:34 +0900

    (querydsl) 기본 Q-Type 활용

--- querydsl/src/test/java/me/choi/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/me/choi/querydsl/QuerydslBasicTest.java
@@ -2,9 +2,7 @@ package me.choi.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.choi.querydsl.entity.Member;
-import me.choi.querydsl.entity.QMember;
 import me.choi.querydsl.entity.Team;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
-import static org.assertj.core.api.Assertions.*;
+import static me.choi.querydsl.entity.QMember.member;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -57,11 +56,9 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl() {
-        QMember m = new QMember("m");
-
-        Member findMember = queryFactory.select(m)
-                                        .from(m)
-                                        .where(m.username.eq("member1"))
+        Member findMember = queryFactory.select(member)
+                                        .from(member)
+                                        .where(member.username.eq("member1"))
                                         .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
