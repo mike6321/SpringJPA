5ccde724f18949611dc1ba352b03dca5c5316cdb
mike6321 <mike6321@naver.com>
2021-08-18 21:32:35 +0900

    (spring-data-jpa) 메소드 이름으로 쿼리 생성

--- spring-data-jpa/src/main/java/me/choi/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/me/choi/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package me.choi.repository;
 import me.choi.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findHelloBy();
 }
