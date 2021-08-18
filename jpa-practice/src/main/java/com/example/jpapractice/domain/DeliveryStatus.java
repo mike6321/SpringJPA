22e232327faa578e2e898d2799ced3bee2c10663
mike6321 <mike6321@naver.com>
2021-08-18 12:52:54 +0900

    (jpa-practice) 엔티티 클래스 개발1

---
+++ jpa-practice/src/main/java/com/example/jpapractice/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpapractice.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ jpa-practice/src/main/java/com/example/jpapractice/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpapractice.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
