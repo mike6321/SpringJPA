e80ed453667838b03fc7cbf28186bd93cdb5808a
mike6321 <mike6321@naver.com>
2021-09-15 09:34:46 +0900

    project init

---
+++ demospringdata/src/main/java/me/choi/DemospringdataApplication.java
@@ -0,0 +1,13 @@
+package me.choi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringdataApplication.class, args);
+    }
+
+}
